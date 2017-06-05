7060328be80768fef2bea42371b05991a26a468a
An <2103887953@qq.com>
2017-06-05 20:00:09 +0800

    解决冲突

--- src/Components/CityType/CinimaSelectNode.js
+++ src/Components/CityType/CinimaSelectNode.js
@@ -14,18 +14,7 @@ class SearchResult extends PureComponent {
 
     var cinimasNodes=[]
     cinimas.forEach(function(node,index){
-<<<<<<< HEAD
       cinimasNodes.push(<li  className={style.linep} key={"key"+index}><Link to={{pathname:'/filmList'}}>{node.name}</Link></li>)
-=======
-      cinimasNodes.push(<Link 
-                          to={{
-                            pathname: '/filmList'
-                          }}
-                          className={ style.link }
-                        >
-                          <li  className={style.linep} key={"key"+index}>{node.name}</li>
-                        </Link>)
->>>>>>> a6722a3013315f8caaaa0688ea6cb652470884c9
     })
 
     return (<div  className={style.sline}>
